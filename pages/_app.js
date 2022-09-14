c41525d7deafabb3b02eb9a82e00f58335b70e46
Vincent <git@vincenttunru.com>
2022-09-14 11:21:14 +0200

    Reproduction of React Aria/Next.js issue
    Based on
    https://codesandbox.io/s/react-spectrum-template-forked-j9ef84

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
+import { SSRProvider } from "@react-aria/ssr";
+import { OverlayProvider } from "@react-aria/overlays";
 import '../styles/globals.css'
+import { useReducer } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [renders, rerender] = useReducer((renders) => renders + 1, 0);
+
+  return (
+    <SSRProvider>
+      <OverlayProvider id="overlayProvider">
+        <Component {...pageProps} />
+        <div style={{height: "50vh", background: "aquamarine"}} onClick={() => setTimeout(rerender, 2000)}>
+          The popover should cover this element. Renders: {renders}. Click me to schedule a re-render in two seconds.
+        </div>
+      </OverlayProvider>
+    </SSRProvider>
+  )
 }
 
 export default MyApp
